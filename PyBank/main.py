cbcf492bf523f2a67c8ecb028f38e601c4db2052
cnzekwe14 <cnzekwe14@yahoo.com>
2022-10-30 12:50:54 -0400

    Python update

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,57 +4,66 @@ import csv
 
 
 csvfile = os.path.join('.','Resources', 'budget_data.csv') # get the path of the data
+with open("PyBank.txt","w") as PyBank_output: # create a text file with the information provided below
+    with open(csvfile,encoding ='utf') as budget_csv: # open the csv file an assign it a variable
+        csvreader = csv.reader(budget_csv, delimiter=",") # read in the budget_data and seperate it by commas
+        print(csvreader) # provide the reader information
 
-with open(csvfile,encoding ='utf') as budget_csv: # open the csv file an assign it a variable
-    csvreader = csv.reader(budget_csv, delimiter=",") # read in the budget_data and seperate it by commas
-    print(csvreader) # provide the reader information
+        header = next(csvreader) # starts data on the next row
+        total_month_count = 0 # store the count of all rows which provides month count
+        budget_1 = [] # store budget values in this list
+        total_budget = 0 # used to get the total sum of budget
+        month_1 = [] # store month in this list
 
-    header = next(csvreader) # starts data on the next row
-    total_month_count = 0 # store the count of all rows which provides month count
-    budget_1 = [] # store budget values in this list
-    total_budget = 0 # used to get the total sum of budget
-    month_1 = [] # store month in this list
-
-    for row in csvreader:
-        total_month_count+=1
-        budget_1.append(row[1])
-        month_1.append(row[0])
+        for row in csvreader:
+            total_month_count+=1
+            budget_1.append(row[1])
+            month_1.append(row[0])
 
 
-    for x in budget_1:
-        total_budget+=  float(x) #sums all data and gets the total
-    
-        
+        for x in budget_1:
+            total_budget+=  float(x) # sums all data and gets the total
+            
+        budget_t = round(total_budget,) # store the total budget to output into text file
         profit_change = round((float(budget_1[len(budget_1) -1]) - float(budget_1[0]))/float(len(budget_1)-1),2) # used to get the average change
-           
+            
 
-    first_number = 0 # looks the first number
-    second_number = 0 # looks at second number
-    profits_review = [] # used to append data to review max and min
-    for i in budget_1:
-        second_number = second_number + 1 # get the next second number
-        if(second_number < len(budget_1)): # second number is less than the total number
-            first_1 = budget_1[first_number] # assign first number 
-            second_1 = budget_1[second_number] # assign second number
-            profits_sub= float(second_1)-float(first_1) # second num - first num to get the gains and losses
-            profits_review.append(profits_sub) # put the gains and losses in a list
-            first_number = first_number + 1 # got to the next first number
-        else:
-            0
+        first_number = 0 # looks the first number
+        second_number = 0 # looks at second number
+        profits_review = [] # used to append data to review max and min
+        for i in budget_1:
+            second_number = second_number + 1 # get the next second number
+            if(second_number < len(budget_1)): # second number is less than the total number
+                first_1 = budget_1[first_number] # assign first number 
+                second_1 = budget_1[second_number] # assign second number
+                profits_sub= float(second_1)-float(first_1) # second num - first num to get the gains and losses
+                profits_review.append(profits_sub) # put the gains and losses in a list
+                first_number = first_number + 1 # got to the next first number
+            else:
+                0
 
-    max_number = round(max(profits_review),) # get the max number in the list of gains and losses   
-    min_number = round(min(profits_review),) # get the min number in the list of gains and losses
-    month_min = profits_review.index(min_number) +1 # find the index of the biggest loss within the list of gains and losses
-    month_max = profits_review.index(max_number) +1 # find the index of the highest gains within the list of gains and losses
-    m1 = month_1[month_max] # get the month that had the highest gains
-    m2 = month_1[month_min] #get the month that had the biggest loss
-   
+        max_number = round(max(profits_review),) # get the max number in the list of gains and losses   
+        min_number = round(min(profits_review),) # get the min number in the list of gains and losses
+        month_min = profits_review.index(min_number) +1 # find the index of the biggest loss within the list of gains and losses
+        month_max = profits_review.index(max_number) +1 # find the index of the highest gains within the list of gains and losses
+        m1 = month_1[month_max] # get the month that had the highest gains
+        m2 = month_1[month_min] #get the month that had the biggest loss
+    
+        # provide the output into the terminal
+        print('Financial Analysis')
+        print('------------------------------')
+        print(f'Total Months: {total_month_count}')   
+        print(f'Total: ${round((total_budget),)}')
+        print(f'Average Change: ${profit_change}')
+        print(f'Increase in Profits: {m1} (${max_number})')
+        print(f'Decrease in Profits: {m2} (${min_number})')
 
-    print('Financial Analysis')
-    print('------------------------------')
-    print(f'Total Months: {total_month_count}')   
-    print(f'Total: ${round((total_budget),)}')
-    print(f'Average Change: ${profit_change}')
-    print(f'Increase in Profits: {m1} (${max_number})')
-    print(f'Decrease in Profits: {m2} (${min_number})')
+        # provide the output into the text file
+        out_1 = f'Financial Analysis\n--------------------------------\nTotal Months: {total_month_count}'
+        out_2 = f'\nTotal: ${budget_t}\nAverage Change: ${profit_change}'
+        out_3 = f'\nIncrease in Profts: "{m1} (${max_number})\nDecrease in Profits: {m2} (${min_number})'
+        PyBank_output.write(out_1) 
+        PyBank_output.write(out_2)
+        PyBank_output.write(out_3)
+    PyBank_output.close()
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,7 +3,7 @@ import csv
 
 csv_path_1 = os.path.join('.','Resources', 'election_data.csv')
 
-with open ("text_3.txt", "w") as output_1:
+with open ("PyPoll.txt", "w") as output_2: # create a texxt file for the output below
     with open(csv_path_1, encoding='utf') as election_csv:
         csvreader = csv.reader(election_csv, delimiter=',')
         print(csvreader)
@@ -11,37 +11,38 @@ with open ("text_3.txt", "w") as output_1:
         header = next(csvreader) # start data analysis in the next row
         election_1 = [] # get all data into a list
         candidates_1 = [] # get the candidates into a list
-        c = []
-        # print("Election Results")
-        a = print("Election Results")
-        # print("-----------------------")
+        print("Election Results")
+        print("-----------------------")
         for row in csvreader:
             election_1.append(row) # put all information into the list
             candidates_1.append(row[2]) # put all candidates names into a list.  This is not distinct
         
-        # print(f'Total Votes: {len(election_1)}') # print the total votes
-        # print("-----------------------")
+        print(f'Total Votes: {len(election_1)}') # print the total votes
+        a = len(election_1)
+        print("-----------------------")
         people = [x for x in set(candidates_1)] # get me the list of candidates.  This is distinct
         # print(people)
         counts = [candidates_1.count(vote) for vote in set(candidates_1)] # count how many time they were selected 
         # print(counts)
         dict_list = dict(zip(people,counts)) # put this in a dictionary to get the candidate with there total count
         sum_counts = sum(counts) # get the total count of votes
+        candidate_part = []
         for i in dict_list: # for key in the list 
             x = ((dict_list[i])/sum_counts)*100 # the key values divided by the sum of all votes
             print(f'{i}:{round(x,3)}% ({dict_list[i]})') # get the candidate percentage and vote
+            candidate_add = f'{i}:{round(x,3)}% ({dict_list[i]})'
+            candidate_part.append(candidate_add)
         print("---------------------")
         winner = max(dict_list, key=dict_list.get) # get the maximum value in the dictionary and returns the key
-        # print(f'Winner: {winner}') # print the winner name
-        # print("---------------------")
-
-        out_1 = "Election Results"
-        out_2 = "------------------"
-        # print("-----------------------"),
-        # print(f'Total Votes: {len(election_1)}'),
-        # print("-----------------------"),
-        # print(f'Winner: {winner}'), 
-        # print("---------------------"))
-    output_1.write(out_1)
-    output_1.write(out_1)
-    output_1.close()
+        print(f'Winner: {winner}') # print the winner name
+        print("-----------------------")
+       
+    # out the results above in a textfile
+    out_1 = f'Election Results\n-----------------\nTotal Votes: {a}\n----------------------\n'
+    output_2.write(out_1)
+    for new_1 in candidate_part:
+        p = new_1 +"\n"
+        output_2.write(p)
+    out_2 = f'-----------------------\nWinner: {winner}\n----------------------'
+    output_2.write(out_2)
+    output_2.close()
